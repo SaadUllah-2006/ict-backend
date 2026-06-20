54979f374c7c92cf600b21a2632ee07891dbf545
Saad <msaadullah.2006@gmail.com>
2026-06-20 14:55:24 +0500

    done

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { register, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('https://ict-frontend-sigma.vercel.app/register', register);
+router.post('https://ict-frontend-sigma.vercel.app/login', login);
 router.get('/me', protect, getMe);
 
 module.exports = router;
